fda65cb81e39f5cc714f9e0605e3fec7c2ab646e
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-11-12 15:45:43 +0700

    fe/init angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,6 +53,8 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users: any;
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe(res => {
+      this.users = res;
+    }, err => {
+        console.log(err);
+    });
+  }
+}
