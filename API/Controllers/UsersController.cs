7ae2bc98643171dab30bbfa4488b725d07c56760
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-12-24 18:06:54 +0700

    extended user entity

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,11 +3,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
   
     public class UsersController : BaseApiController
     {
